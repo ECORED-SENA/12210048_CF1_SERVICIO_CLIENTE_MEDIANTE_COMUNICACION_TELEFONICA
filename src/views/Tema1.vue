872384f47307e69c2915968a53912d6a1dad3d2c
Henry Álvarez Astudillo <halvarezast@gmail.com>
2026-02-21 12:01:56 -0500

    Update1201

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -52,6 +52,79 @@
         .card-cualidad Empatía
       .col-auto
         .card-cualidad Paciencia
+  
+  
+      .bg-full-width
+        .px-4.px-md-5.pb-md-3
+
+             
+      .carousel-bg
+        .row.justify-content-center.mb-5.align-items-center
+          .col-lg-9.mb-3.mb-lg-0.order-lg-2
+            SlyderF(columnas="col-md-6 col-xl-6")
+              .bg-color-white.tarjeta.p-4.h-100.shadow-app.text-center.bordes
+                img(src='@/assets/curso/tema1/carrusel_02.svg' alt='AvatarTop' , style="max-width: 90px").mx-auto.mb-3
+                p Capacidad de transmitir ideas.
+              .bg-color-white.tarjeta.p-4.h-100.shadow-app.text-center.bordes
+                img(src='@/assets/curso/tema1/carrusel_03.svg' alt='AvatarTop' , style="max-width: 90px").mx-auto.mb-3
+                p Capacidad de atraer la atención del interlocutor.
+              .bg-color-white.tarjeta.p-4.h-100.shadow-app.text-center.bordes
+                img(src='@/assets/curso/tema1/carrusel_04.svg' alt='AvatarTop' , style="max-width: 90px").mx-auto.mb-3
+                p Capacidad de atención.
+              .bg-color-white.tarjeta.p-4.h-100.shadow-app.text-center.bordes
+                img(src='@/assets/curso/tema1/carrusel_05.svg' alt='AvatarTop' , style="max-width: 90px").mx-auto.mb-3
+                p Habilidad para captar las necesidades del cliente.
+              .bg-color-white.tarjeta.p-4.h-100.shadow-app.text-center.bordes
+                img(src='@/assets/curso/tema1/carrusel_06.svg' alt='AvatarTop' , style="max-width: 90px").mx-auto.mb-3
+                p Alto sentido de compromiso.
+              .bg-color-white.tarjeta.p-4.h-100.shadow-app.text-center.bordes
+                img(src='@/assets/curso/tema1/carrusel_07.svg' alt='AvatarTop' , style="max-width: 90px").mx-auto.mb-3
+                p Capacidad de organización.
+          .col-lg-3.order-lg-1.d-none.d-lg-block
+              figure
+                img(src='@/assets/curso/tema1/carrusel_01.png', style="max-width: 250px", alt='Imagen de apoyo', data-aos="zoom-in").m-auto
+      
+
+    .row.mb-5
+      p.mb-0 Además de estas cualidades, la paciencia es un factor fundamental en esta profesión. El asesor telefónico se enfrenta a todo tipo de situaciones, algunas tan inusuales que resultan “difíciles de creer”. A lo largo de su experiencia laboral, vivirá momentos positivos y negativos; sin embargo, si cuenta con las habilidades adecuadas y una disposición genuina para ofrecer un buen servicio, su desempeño será exitoso.Todas las personas pueden ser capacitadas para desempeñar tareas que exijan un perfil comercial y de servicio, siempre que tengan las cualidades necesarias o estén dispuestas a desarrollarlas con esfuerzo y dedicación. Aunque algunas de estas habilidades pueden estar presentes de manera natural, en muchos casos deben ser cultivadas.
+
+
+
+    .bg-full-width
+      .px-4.px-md-5.pb-md-3
+
+      .titulo-con-imagen.mb-5(data-aos="fade-right")
+        .titulo-con-imagen__fondo
+          img.titulo-con-imagen__imagen(src="@/assets/curso/tema1/imagen-titulos.svg")
+          h3.titulo-con-imagen__texto El cliente es lo primero
+
+    .row.mb-2
+          p.mb-0 Es fundamental partir de la premisa de que el cliente es la persona más importante de un negocio, empresa u organización. Toda persona que aspire a ingresar a este tipo de entorno laboral debe tener claro que el cliente constituye el centro de la actividad empresarial, y que, en muchos casos, serán los asesores telefónicos el primer contacto que un posible consumidor tendrá con la organización.
+
+
+    .row.mb-10
+      .col-lg-5.mb-3.mb-lg-0
+        figure
+          img(src="@/assets/curso/tema1/3.png", data-aos="zoom-in")
+      .col-lg-7
+        .row.align-items-start.mb-3(data-aos="fade-left")
+          .col-lg
+          p.mb-0 En este sentido, resulta imprescindible recordar que el elemento vital para una comunicación efectiva es la práctica de los modales y las buenas costumbres, ya que estas permiten evidenciar la importancia que tiene el interlocutor.
+        .row.align-items-start.mb-10(data-aos="fade-left")  
+          .col-lg-auto
+            img.d-none.d-lg-flex(src="@/assets/curso/tema1/4.svg", style="max-width: 90px").mx-auto
+          .col-lg
+            p(data-aos="fade-left") Por ello, el asesor o asesora telefónica debe poner de manifiesto sus principios y valores, los cuales se reflejan directamente en la calidad del servicio ofrecido. Una comunicación telefónica adecuada contribuye de manera significativa al desarrollo de un óptimo servicio al cliente, fortaleciendo la percepción positiva de la empresa.
+
+
+      .row.mt-20.text-center.justify-content-center.align-items-center
+        .col-lg-6
+          .titulo-sexto.color-acento-contenido
+            p <b>Figura 1.</b> Pérdida de clientes por mala atención.
+          figure
+            img(src="@/assets/curso/tema1/figura1.svg", data-aos="zoom-in")
+
+
 
 </template>
 
